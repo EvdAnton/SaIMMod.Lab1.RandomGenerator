772f8330c660e3818849f28a26182423f4a9ef9b
Anton Evdakov <aevdakov@AM.IDTCORP.NET>
2020-09-17 12:57:30 +0300

    [SaIMMod - 1] feat(init): Add main functionality

---
+++ Lab1.RandomGenerator/Generator.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1.RandomGenerator
+{
+    public class IntervalWithCountOfNumbers
+    {
+        public float Range { get; set; }
+        public int InnerNumbers { get; set; }
+    }
+
+    public class Generator
+    {
+
+        public int Period { get; private set; }
+        public List<int> APeriod { get; private set; }
+        public float Dispersion { get; private set; }
+
+        public float Expectation { get; private set; }
+
+        public float SigmaLimit { get; private set; }
+
+       
+        private uint From { get; set; }
+        private uint To { get; set; }
+        
+        public List<List<float>> Result { get; }
+        
+        private readonly uint _startValueX0;
+        private readonly uint _countOfElements;
+        private readonly uint _multiplier;
+        private readonly uint _mod;
+
+        public Generator(uint startValueX0, uint multiplier, uint mod, uint from,
+            uint to, uint countOfElements)
+        {
+            _countOfElements = countOfElements;
+            _startValueX0 = startValueX0;
+            _multiplier = multiplier;
+            _mod = mod;
+
+            Result = new List<List<float>>()
+            {
+                new List<float>()
+            };
+        
+            From = from;
+            To = to;
+        }
+
+        public List<IntervalWithCountOfNumbers> GetResult()
+        {
+            GenerateValue(_startValueX0);
+
+            Expectation = GetExpectation();
+            Dispersion = GetDispersion();
+            SigmaLimit = GetSigmaLimit();
+            Period = GetPeriod();
+            APeriod = GetAllAperiods().ToList();
+
+            return GetFrequency();
+        }
+
+        private void GenerateValue(uint currentValue)
+        {
+            for(var i = 0; i < _countOfElements; i++)
+            {
+                var nextValue = _multiplier * currentValue % _mod;
+                var addedValue = (float)nextValue / _mod;
+
+                if (Result.Last().Count > 0 && Result.Last().First().Equals(addedValue))
+                {
+                    Result.Add(new List<float>());
+                }
+
+                Result.Last().Add(addedValue);
+                
+                currentValue = nextValue;
+            }
+        }
+
+        private float GetExpectation()
+        {
+            var count = Result.Count;
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            float expectation = 0;
+
+            foreach (var result in Result)
+            {
+                expectation += GetExpectationForOneSequence(result);
+            }
+
+            expectation /=  count;
+
+            return expectation;
+        }
+
+        private float GetExpectationForOneSequence(List<float> result)
+        {
+            var count = result.Count;
+            if (count == 0)
+            {
+                return 0;
+            }
+            
+            var sum = result.Sum();
+            var expectation = sum / count;
+            
+            return expectation;
+        }
+
+        private float GetDispersion()
+        {
+            var count = Result.Count;
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            float expectation = 0;
+
+            foreach (var result in Result)
+            {
+                expectation += GetDispersionForOneSequence(result);
+            }
+
+            expectation /= count;
+
+            return expectation;
+        }
+
+        private float GetDispersionForOneSequence(List<float> result)
+        {
+            var expectation = Expectation;
+            
+            if (expectation.Equals(0))
+            {
+                return 0;
+            }
+
+            var count = result.Count; 
+            var sum = (float)result.Sum(value => Math.Pow(value - expectation, 2));
+            var dispersion = sum / count;
+            
+            return dispersion;
+        }
+
+        private float GetSigmaLimit()
+        {
+            var sigmaLimit = (float)Math.Sqrt(Dispersion);
+            
+            return sigmaLimit;
+        }
+
+        private List<float> GetAllNumbersFromResult(List<List<float>> results)
+        {
+            var resultList = new List<float>();
+
+            foreach(var result in results)
+            {
+                resultList.AddRange(result);
+            }
+
+            return resultList;
+        }
+
+        private List<IntervalWithCountOfNumbers> GetFrequency()
+        {
+            var result = GetAllNumbersFromResult(Result);
+
+            var countOfInterval = 20;
+            float currentInterval = From;
+            float previousInterval = From;
+            var range = To - From;
+            var step = (float)range / countOfInterval;
+
+            var intervals = new List<IntervalWithCountOfNumbers>();
+
+            while(currentInterval <= To)
+            {
+                previousInterval = currentInterval;
+                currentInterval += step;
+
+                var countOfNumersInInterval = result.Count(value => value < currentInterval && value >= previousInterval);
+
+                intervals.Add(new IntervalWithCountOfNumbers 
+                {
+                    Range = currentInterval, 
+                    InnerNumbers = countOfNumersInInterval 
+                });
+            }
+
+            return intervals;
+        }
+
+        private int GetPeriod()
+        {
+            var countOfElementsInEveryPeriod = Result.Select(result => result.Count).ToList();
+
+            int period = 0;
+            int maxMatches = 0;
+
+            foreach (var item in countOfElementsInEveryPeriod)
+            {
+                int matches = countOfElementsInEveryPeriod.Count(i => i == item);
+
+                if (matches > maxMatches)
+                {
+                    maxMatches = matches;
+                    period = item;
+                }
+            }
+
+            return period;
+        }
+
+        private IEnumerable<int> GetAllAperiods()
+        {
+            if(Period == default)
+            {
+                Period = GetPeriod();
+            }
+
+            var countOfElementsInEveryPeriod = Result.Select(result => result.Count).ToList();
+
+            var aperiods = countOfElementsInEveryPeriod.Where(period => period != Period);
+
+            return aperiods;
+        }
+
+        public List<float> GetValuesFromPeriod(int countOfElementsInPeriod)
+        {
+            var period = Result.FirstOrDefault(result => result.Count == countOfElementsInPeriod);
+
+            return period ?? new List<float>();
+        }
+    }
+}
---
+++ Lab1.RandomGenerator/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using Syncfusion.UI.Xaml.Charts;
+using System;
+using System.Windows;
+
+namespace Lab1.RandomGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private uint From { get; set; }
+        private uint To { get; set; }
+        private uint Multiplier { get; set; }
+        private uint Mod { get; set; }
+        private uint StartValue { get; set; }
+        private uint CountOfElementsTextBox { get; set; }
+        
+        private static void VerifyThatIneNumberMortThenOther(uint bigger, uint less)
+        {
+            if (less > bigger)
+            {
+                throw new ArgumentOutOfRangeException($"{bigger} should be less then {less}");
+            }
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            To = Convert.ToUInt32(toTextBox.Text);
+            From = Convert.ToUInt32(fromTextBox.Text);
+
+            Mod = Convert.ToUInt32(modTextBox.Text);
+            StartValue = Convert.ToUInt32(startValueTextBox.Text);
+            Multiplier = Convert.ToUInt32(multiplierTextBox.Text);
+            CountOfElementsTextBox = Convert.ToUInt32(countOfElementsTextBox.Text);
+
+            VerifyThatIneNumberMortThenOther(To, From);
+            VerifyThatIneNumberMortThenOther(Mod, Multiplier);
+
+            var generator = new Generator(StartValue, Multiplier, Mod, From, To, CountOfElementsTextBox);
+
+            CreateChart(generator);
+
+            dispersionTextBox.Text = generator.Dispersion.ToString();
+            expectationTextBox.Text = generator.Expectation.ToString();
+            sigmaLimitTextBox.Text = generator.SigmaLimit.ToString();
+
+            ShowResult(generator);
+        }
+
+        private static void ShowResult(Generator generator)
+        {
+            var window = new ShowPeriodWindow();
+
+            var period = generator.Period;
+            var aperiods = generator.APeriod;
+
+            GetValuesFromPeriod(period, generator, window, true);
+
+            foreach (var aperiod in aperiods)
+            {
+                GetValuesFromPeriod(aperiod, generator, window, false);
+            }
+
+            window.Show();
+        }
+
+
+        private static void GetValuesFromPeriod(int period, Generator generator, ShowPeriodWindow window, bool isPeriod)
+        {
+            var values = generator.GetValuesFromPeriod(period);
+
+            if (isPeriod)
+            {
+                foreach (var value in values)
+                {
+                    window.PeriodTextBlock.Text += value + "  ";
+                }
+
+                window.PeriodCountTextBlock.Text = period.ToString();
+            }
+            else
+            {
+                foreach (var value in values)
+                {
+                    window.APeriodTextBlock.Text += value + "  ";
+                }
+
+                window.APeriodTextBlock.Text += ";   ";
+
+                window.APeriodCountTextBlock.Text = period.ToString();
+            }
+        }
+
+        private void CreateChart(Generator generator)
+        {
+            MyChart.Series.Clear();
+
+            //Adding Legends for the chart
+            var legend = new ChartLegend();
+            MyChart.Legend = legend;
+
+            //Initializing column series
+            var series = new ColumnSeries
+            {
+                ItemsSource = generator.GetResult(),
+                XBindingPath = "Range",
+                YBindingPath = "InnerNumbers",
+                ShowTooltip = true,
+                Label = "InnerNumbers"
+            };
+            series.Label = "Range";
+
+            //Setting adornment to the chart series
+            series.AdornmentsInfo = new ChartAdornmentInfo() { ShowLabel = true };
+
+            //Adding Series to the Chart Series Collection
+            MyChart.Series.Add(series);
+        }
+    }
+}
